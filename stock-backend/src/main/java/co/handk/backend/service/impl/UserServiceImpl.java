fe019824dda3cca4f5ddc12b6f30fbdf41cc9402
chinatatsu <shenalex25@gmail.com>
2026-03-27 14:42:31 +0900

    fix: add validation "username and password is not null"

--- stock-backend/src/main/java/co/handk/backend/service/impl/UserServiceImpl.java
+++ stock-backend/src/main/java/co/handk/backend/service/impl/UserServiceImpl.java
@@ -36,6 +36,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     public LoginVO login(LoginDTO dto) {
         String username = dto.getUsername();
         String rawPassword = dto.getPassword();
+        if (StringUtils.isBlank(username) || StringUtils.isBlank(rawPassword)) {
+            throw new RuntimeException("用户名或密码不能为空");
+        }
         User user = userMapper.selectByUsername(username);
         if (user == null) {
             throw new RuntimeException("用户不存在");
